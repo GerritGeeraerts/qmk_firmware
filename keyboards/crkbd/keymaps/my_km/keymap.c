8d842f7272551c55eda45f79af122b7cbc523381
Gerrit Geeraerts <ggeeraer@gmail.com>
2021-11-09 20:33:06 +0100

    c

--- keyboards/crkbd/keymaps/my_km/keymap.c
+++ keyboards/crkbd/keymaps/my_km/keymap.c
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+qmk flash -kb crkbd/rev1/common -km my_km
 */
 
 #include QMK_KEYBOARD_H
