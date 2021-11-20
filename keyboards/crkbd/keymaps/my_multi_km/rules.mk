6b2bf73bbc3f7e886fc9be4685f40ba9761c0c97
Gerrit Geeraerts <ggeeraer@gmail.com>
2021-11-20 17:51:43 +0100

    c

--- keyboards/crkbd/keymaps/my_multi_km/rules.mk
+++ keyboards/crkbd/keymaps/my_multi_km/rules.mk
@@ -1,8 +1,10 @@
 # For Elite-C
 BOOTLOADER = atmel-dfu
+RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
 
 # https://www.reddit.com/r/olkb/comments/9pyc0u/qmk_media_keys_are_not_working/
-EXTRAKEY_ENABLE = yes
+EXTRAKEY_ENABLE = no
+MOUSEKEY_ENABLE = no
 
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
